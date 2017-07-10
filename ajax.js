3d4214494c5f946a426fddc0c63f51635053ac3c
Doug <doug.steinberg@gmail.com>
2017-07-10 16:08:23 -0400

    Ping request part 2 partially copmlete

--- ajax.js
+++ ajax.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-  $(function() {
+
   //  DECLARE VARIABLES
   var button = document.getElementById( 'get_root' );
-
+  var pingRequest = document.getElementById( 'ping' );
   // REGISTER EVENT HANDLERS
 
   button.addEventListener( 'click', function() {
@@ -17,5 +17,16 @@ document.addEventListener("DOMContentLoaded", function() {
         dataType: 'html',
       });
     });
+
+  pingRequest.addEventListener( 'click', function() {
+    console.log( 'CLICKED GET ROOT BUTTON' );
+    console.log( 'BEFORE AJAX REQUEST' );
+    // MAKE AJAX REQUEST HERE
+
+  $.ajax( {
+        url: 'http://first-ajax-api.herokuapp.com/ping',
+        method: 'GET',
+        dataType: 'html',
+      });
   });
 });
