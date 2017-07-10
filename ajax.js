6105ed57aa3f81f6e8b831cd20f40e8ff803898b
Doug <doug.steinberg@gmail.com>
2017-07-10 15:45:12 -0400

    Added link to root api step 1

--- ajax.js
+++ ajax.js
@@ -1,5 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+  $(function() {
+  //  DECLARE VARIABLES
+  var button = document.getElementById( 'get_root' );
 
-  /* Your code goes here */
+  // REGISTER EVENT HANDLERS
 
+  button.addEventListener( 'click', function() {
+    console.log( 'CLICKED GET ROOT BUTTON' );
+    console.log( 'BEFORE AJAX REQUEST' );
+
+    // MAKE AJAX REQUEST HERE
+
+  $.ajax( {
+        url: 'http://first-ajax-api.herokuapp.com/',
+        method: 'GET',
+        dataType: 'html',
+      });
+    });
+  });
 });
